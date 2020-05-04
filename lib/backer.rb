80b6e90f134d8c83e675a61e5bf49d25a301e23b
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-05-04 13:15:00 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,8 +7,8 @@ class Backer
     @backed_projects = []
   end 
   
-  def back_project(project)
-    @backed_projects << Project.new(project) 
+  def back_project(title)
+    @backed_projects << Project.new(title) 
   end 
   
 end 
