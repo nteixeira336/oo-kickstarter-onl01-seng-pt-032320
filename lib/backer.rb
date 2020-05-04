0f7900132435978955f651db1079dac29bdee30c
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-05-04 13:12:29 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,9 @@
 class Backer 
   
-  attr_reader :backed_projects 
+  attr_reader :backed_projects, :name  
   
-  def initialize
+  def initialize(name)
+    @name = name 
     @backed_projects = []
   end 
   
